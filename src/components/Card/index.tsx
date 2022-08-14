0a84d3c1fc0f90fc79bdb971ad70680f2951e7e7
Alicia Merlino <alicia@beacon.co.uk>
2022-08-14 16:00:42 +0100

    Create basic card component with different colors for request and response

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { IMessage, MessageTypes } from "../../types/messages";
 import "./styles.css";
 
 type ICardProps = {
-  type: "response" | "query";
+  message: IMessage;
 };
 
-const Card = ({ type }: ICardProps) => {
-  const isResponseCard = type === "response";
-  return <div className={isResponseCard ? "card-response" : "card-query"} />;
+const Card = ({ message }: ICardProps) => {
+  const isResponseCard = message.type === MessageTypes.MESSAGE_TYPE_RESPONSE;
+  return (
+    <div className={isResponseCard ? "card-response" : "card-query"}>
+      <p>{message.content}</p>
+    </div>
+  );
 };
 
 export default Card;
